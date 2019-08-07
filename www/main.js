271cfd46d392ef7dfac51fefdb683c8fa4f290b2
Zhenya <demichka@gmail.com>
2019-08-07 08:21:12 +0200

    Added task in eng and swe and comments as well

--- www/main.js
+++ www/main.js
@@ -1,23 +1,35 @@
 let cards = new CardDeck();
 let gameBoard = new GameBoard(cards);
+
+//Part 1
+//new card deck
 console.log("cards should be in right order: ", gameBoard.cardDeck);
+
+//shuffle card deck
 let shuffled = gameBoard.shuffleCardDeck(gameBoard.cardDeck.slice());
 console.log("shuffled: ", shuffled);
+
+//Part 2
+//add players
 let slim = new Player("Slim");
 let luke = new Player("Luke");
-// let mike = new Player("Mike");
 
 gameBoard.addPlayer(slim);
 gameBoard.addPlayer(luke);
-// gameBoard.addPlayer(mike);
 
 console.log("these players are added:");
 
 for (let player of gameBoard.players) {
   console.log(player.name);
 }
+
+
+//Take a copy of shuffled card deck to prevent changes in initial one
 let cardsInGame = shuffled.slice();
+
+//deal cards to players
 gameBoard.dealCards(cardsInGame, 5);
+
 
 let showCards = (player) => {
   for (let card of player.playersCards) {
@@ -35,6 +47,8 @@ for (let player of gameBoard.players) {
 console.log(cardsInGame, "rest of cards");
 console.log("************************");
 
+//Part 3
+//players throw cards and take another two from card deck
 for (let player of gameBoard.players) {
   gameBoard.playerPutCards(player, 2);
   gameBoard.playerTakeCards(cardsInGame, player, 2);
@@ -43,10 +57,14 @@ for (let player of gameBoard.players) {
 console.log(cardsInGame, "rest of cards");
 console.log("************************");
 
+
+//Part 4
+//players throw all their cards to the throwing pile
 for (let player of gameBoard.players) {
   gameBoard.playerPutCards(player, player.playersCards.length);
 }
 
+//throwing pile returns to the card deck
 cardsInGame = cardsInGame.concat(gameBoard.stack);
 let restInStack = gameBoard.stack.slice();
 console.log(restInStack), "Stack before clean";
